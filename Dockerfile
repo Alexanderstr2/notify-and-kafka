e432da3880e5873155579a049612ada164b37162
Alexander Stratmoen <alexanderstratmoen@Alexanders-MacBook-Pro.local>
2024-06-19 10:17:37 -0700

    Added kafkacat

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM alpine:latest
 
-RUN apk add --no-cache inotify-tools bash
+RUN apk add --no-cache inotify-tools bash kafkacat
 
 COPY watcher.sh /watcher.sh
 
 RUN chmod +x /watcher.sh
+
+# Set default environment variables
+ENV KAFKA_BROKERS=localhost:9092
+ENV KAFKA_TOPIC=default_topic
+ENV EVENTS=modify,create,delete
+ENV DIRECTORY=/data
 
 CMD ["/watcher.sh"]
--- watcher.sh
+++ watcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if necessary environment variables are set
 if [[ -z "${DIRECTORY}" ]]; then
   echo "You must specify the directory to watch."
   exit 1
@@ -10,13 +11,26 @@ if [[ ! -d "${DIRECTORY}" ]]; then
   exit 2
 fi
 
-if [[ -z "${COMMAND}" ]]; then
-  echo "You must specify a command to execute."
+if [[ -z "${KAFKA_BROKERS}" ]]; then
+  echo "You must specify Kafka brokers."
   exit 3
 fi
 
+if [[ -z "${KAFKA_TOPIC}" ]]; then
+  echo "You must specify a Kafka topic."
+  exit 4
+fi
+
+if [[ -z "${EVENTS}" ]]; then
+  echo "You must specify the file system events to monitor."
+  exit 5
+fi
+
+# Main loop to watch directory and send events to Kafka
 while true; do
-  inotifywait -r -e "${EVENTS}" "${DIRECTORY}"
-  eval "${COMMAND}"
+  inotifywait -r -e "${EVENTS}" "${DIRECTORY}" | while read path action file; do
+    echo "Event $action occurred on $file in $path" | \
+    kafkacat -b "${KAFKA_BROKERS}" -t "${KAFKA_TOPIC}" -P
+  done
   sleep 1
 done
